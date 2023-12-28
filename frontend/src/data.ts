f7461de1a897cbc79764f36a05fb6cb6c7640608
tammchannn <anhtamtat2611@gmail.com>
2023-12-28 10:12:45 +0700

    header, footer

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -16,7 +16,7 @@ export const BANNERS: Banner[] = [
 export const PRODUCTS: Product[] = [
     {
         imageUrl: productPath + '1_thumb.png',
-        name: "Toy's name",
+        name: "bút màu",
         category: 'Toy',
         price: 199.999,
         id: 1,
@@ -27,7 +27,7 @@ export const PRODUCTS: Product[] = [
     },
     {
         imageUrl: productPath + '1b.jpg',
-        name: "Toy's name",
+        name: "gấu bông",
         category: 'Toy',
         price: 199.999,
         id: 2,
