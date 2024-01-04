f81b483cfc4e074ca7cd0f462449540b70ee8ae5
Uyen <nnphuyen563@gmail.com>
2024-01-05 00:56:10 +0700

    setup

---
+++ backend/src/data.ts
@@ -0,0 +1,309 @@
+
+const productPath = 'assets/img/product/'
+const catagoryPath ='assets/img/catagory/'
+
+export const CATAGORYS: any[] = [
+    {
+        imageUrl: catagoryPath + 'action.jpg',
+        name: "Hành động",
+    },
+    {
+        imageUrl: catagoryPath + 'art.jpg',
+        name: "Mỹ thuật"
+    },
+    {
+        imageUrl: catagoryPath + 'boardgame.jpg',
+        name: "Board Game",
+    },
+    {
+        imageUrl: catagoryPath + 'collect.jpg',
+        name: "Bộ sưu tập"
+    },
+    {
+        imageUrl: catagoryPath + 'doll.jpg',
+        name: "Búp bê",
+    },
+    {
+        imageUrl: catagoryPath + 'outdoor.jpg',
+        name: "Trò chơi ngoài trời"
+    }
+]
+export const PRODUCTS: any[] = [
+    {
+        imageUrl: productPath + '1_thumb.png',
+        name: "Hộp màu",
+        category: 'Mỹ thuật',
+        price: 199.999,
+        id: 1,
+        description: 'Description of product 1',
+        star: 3,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '1_thumb.png',
+        name: "Hộp màu 1",
+        category: 'Mỹ thuật',
+        price: 129.000,
+        id: 1,
+        description: 'Description of product 1',
+        star: 4,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '1_thumb.png',
+        name: "Hộp màu 2",
+        category: 'Mỹ thuật',
+        price: 149.000,
+        id: 1,
+        description: 'Description of product 1',
+        star: 2,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '1_thumb.png',
+        name: "Hộp màu 3",
+        category: 'Mỹ thuật',
+        price: 159.000,
+        id: 1,
+        description: 'Description of product 1',
+        star: 4,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '1b.jpg',
+        name: "Toy's name",
+        category: 'Con bo',
+        price: 199.999,
+        id: 1,
+        description: 'Description of product 2',
+        star: 3.5,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '1c.png',
+        name: "Toy's name",
+        category: 'Xe do choi',
+        price: 199.999,
+        id: 1,
+        description: 'Description of product 3',
+        star: 4.2,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '1d.jpg',
+        name: "Toy's name",
+        category: 'Xe do choi',
+        price: 199.999,
+        id: 1,
+        description: 'Description of product 4',
+        star: 4.8,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+
+    },
+    {
+        imageUrl: productPath + '1e.png',
+        name: "Toy's name",
+        category: 'Toy',
+        price: 199.999,
+        id: 1,
+        description: 'Description of product 5',
+        star: 4.1,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '1f.png',
+        name: "Toy's name",
+        category: 'Toy',
+        price: 199.999,
+        id: 1,
+        description: 'Description of product 1f',
+        star: 4.0,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '2_thumb.png',
+        name: "Toy's name",
+        category: 'Con bo',
+        price: 199.999,
+        id: 2,
+        description: 'Description of product 2a',
+        star: 3.8,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '3_thumb.jpg',
+        name: "Toy's name",
+        category: 'Toy',
+        price: 199.999,
+        id: 3,
+        description: 'Description of product 3a',
+        star: 4.5,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '4_thumb.jpg',
+        name: "Toy's name",
+        category: 'Toy',
+        price: 199.999,
+        id: 4,
+        description: 'Description of product 4a',
+        star: 4.2,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '5_thumb.png',
+        name: "Toy's name",
+        category: 'Toy',
+        price: 199.999,
+        id: 5,
+        description: 'Description of product 5a',
+        star: 4.7,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '6_thumb.png',
+        name: "Toy's name",
+        category: 'Toy',
+        price: 199.999,
+        id: 6,
+        description: 'Description of product 6a',
+        star: 4.1,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '7_thumb.png',
+        name: "Toy's name",
+        category: 'Toy',
+        price: 199.999,
+        id: 7,
+        description: 'Description of product 7a',
+        star: 4.6,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '8_thumb.jpg',
+        name: "Toy's name",
+        category: 'Toy',
+        price: 199.999,
+        id: 8,
+        description: 'Description of product 8a',
+        star: 4.3,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '12_thumb.png',
+        name: "Toy's name",
+        category: 'Toy',
+        price: 199.999,
+        id: 12,
+        description: 'Description of product 12a',
+        star: 3.9,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '19_thumb.png',
+        name: "Toy's name",
+        category: 'Hóa trang',
+        price: 199.999,
+        id: 19,
+        description: 'Description of product 19a',
+        star: 4.8,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '19b.png',
+        name: "Toy's name",
+        category: 'Toy',
+        price: 199.999,
+        id: 19,
+        description: 'Description of product 19b',
+        star: 4.4,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '19c.png',
+        name: "Toy's name",
+        category: 'Toy',
+        price: 199.999,
+        id: 19,
+        description: 'Description of product 19c',
+        star: 4.6,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '20_thumb.png',
+        name: "Toy's name",
+        category: 'Toy',
+        price: 199.999,
+        id: 20,
+        description: 'Description of product 20a',
+        star: 4.2,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '20b.png',
+        name: "Toy's name",
+        category: 'Toy',
+        price: 199.999,
+        id: 20,
+        description: 'Description of product 20b',
+        star: 4.7,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+    {
+        imageUrl: productPath + '20c.jpg',
+        name: "Toy's name",
+        category: 'Toy',
+        price: 199.999,
+        id: 20,
+        description: 'Description of product 20c',
+        star: 4.1,
+        stock: 50,
+        time: new Date(),
+        quantity: 10
+    },
+];
